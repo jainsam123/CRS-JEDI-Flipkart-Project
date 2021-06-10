7955541e391fe789ab843d386a16c15919a675a1
Sameer Jain <sameer2000jain12@gmail.com>
2021-06-11 01:00:01 +0530

    message

---
+++ CMSProject/src/com/flipkart/constant/NotificationType.java
@@ -0,0 +1,10 @@
+package com.flipkart.constant;
+/**
+ * 
+ * @author sameer
+ * Enumeration class for Notification Types
+ *
+ */
+public enum NotificationType {
+	REGISTRATION,REGISTRATION_APPROVAL,PAYMENT;
+}
